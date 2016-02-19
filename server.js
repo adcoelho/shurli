a75289d01df18b5f516aa81813ced1e721eb4be2
adcoelho <antoniodcoelho@gmail.com>
2016-02-19 16:28:16 +0000

    Added some additional logic

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 var express = require('express');
+var url = require('url');
+var validUrl = require('valid-url');
 var app = express();
+
+var url_list = [];
+
+var addUrl = function (url) {
+    url_list.push(url);
+    return url_list.length - 1;
+}
 
 app.get('/(\\d+)', function (req, res) {
     res.end(req.url);
 });
 
 app.get('/new/*', function (req, res) {
-    res.end(req.url);
+    var urlObj = url.parse(req.url, true);
+    var originalUrl = urlObj.pathname.substring(5); // remove '/new/'
+    var result = {}
+    if (validUrl.isUri(originalUrl) || urlObj.query['allow'] === "true"){
+        result.original_url = originalUrl;
+        result.short_url = '';
+    } else {
+        result = {error: 'Invalid Url'};
+    }
+    res.end(JSON.stringify(result));
 });
 
 //app.listen(process.env.PORT || 5000);
